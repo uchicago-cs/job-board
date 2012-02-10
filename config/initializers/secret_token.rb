fb7686cc511785fad3ce540b6c168a0816a998d1
Borja Sotomayor <borja@cs.uchicago.edu>
2012-02-10 16:57:50 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechJobs::Application.config.secret_token = 'adb25d819448ec0569e0ca1ee5c0b15809b1c4e7c4ddbfab4efe7e92981f35e28e80d629e532c9ca7b9159b98d1f3b43748e5917c21a70332b7968595dd48958'
