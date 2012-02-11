e0a6a111611fb531cdedfe3fda109004fa8b5803
Borja Sotomayor <borja@cs.uchicago.edu>
2012-02-11 17:46:53 -0600

    Modified postings form to support employer submission and admin review

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -38,9 +38,20 @@ class JobPostingsController < ApplicationController
   def create
     @job_posting = JobPosting.new(params[:job_posting])
 
+    if current_user.type == "Employer"
+       @job_posting.employer_id = current_user.id
+       @job_posting.state = "Pending Approval"
+       notice = "Your job posting has been submitted and is pending approval. You will be notified via e-mail once it has been reviewed by an administrator."
+       next_page = @job_posting
+    elsif current_user.type == "Administrator" 
+       @job_posting.state = "Approved"
+       notice = "Job posting was successfully created."
+       next_page = @job_posting
+    end
+
     respond_to do |format|
       if @job_posting.save
-        format.html { redirect_to @job_posting, notice: 'Job posting was successfully created.' }
+        format.html { redirect_to next_page, notice: notice }
         format.json { render json: @job_posting, status: :created, location: @job_posting }
       else
         format.html { render action: "new" }
@@ -52,9 +63,33 @@ class JobPostingsController < ApplicationController
   def update
     @job_posting = JobPosting.find(params[:id])
 
+
+    if current_user.type == "Employer"
+       params[:job_posting][:state] = "Pending Approval"
+       notice = "Your job posting has been updated. Your changes will have to be approved by an administrator before the updated posting is published."
+        next_page = @job_posting
+    elsif current_user.type == "Administrator" 
+      if params[:reject]
+        params[:job_posting][:state] = "Rejected"
+        notice = 'Job posting was rejected.'
+        next_page = dashboard_path
+      elsif params[:request_changes]
+        params[:job_posting][:state] = "Changes needed"
+        notice = 'Submitter has been notified that the job posting should be changed.'
+        next_page = dashboard_path
+      elsif params[:approve]
+        params[:job_posting][:state] = "Approved"
+        notice = 'Job posting was approved.'
+        next_page = dashboard_path
+      else     
+        notice = 'Job posting was successfully updated.'
+        next_page = @job_posting
+      end
+    end
+
     respond_to do |format|
       if @job_posting.update_attributes(params[:job_posting])
-        format.html { redirect_to @job_posting, notice: 'Job posting was successfully updated.' }
+        format.html { redirect_to next_page, notice: notice }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -68,7 +103,7 @@ class JobPostingsController < ApplicationController
     @job_posting.destroy
 
     respond_to do |format|
-      format.html { redirect_to job_postings_url }
+      format.html { redirect_to request.referer }
       format.json { head :no_content }
     end
   end
