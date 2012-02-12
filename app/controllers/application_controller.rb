5489e49ecf0f8829aa436e47a415d5d398f53071
Borja Sotomayor <borja@cs.uchicago.edu>
2012-02-11 20:19:06 -0600

    Redirect to dashboard after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)  
+    stored_location_for(resource) || dashboard_path
+  end
+
 end
