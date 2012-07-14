78f97e90d553ce7897a812ed5d93308fccec82f1
Martin Posthumus <mposthumus@uchicago.edu>
2012-07-14 14:19:54 -0500

    Adding basic config for LDAP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   protect_from_forgery
 
   def after_sign_in_path_for(resource)  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :ldap_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :email, :password, :password_confirmation, :remember_me, :firstname, :lastname, :type, :Full_Time, :Part_Time, :Internship
   has_and_belongs_to_many :tags
+
+  validate :is_in_ldap
+
+  def is_in_ldap
+    unless Devise::LdapAdapter.get_ldap_param(self.username, "uid")
+      errors.add(:username, "is not a valid CNetID")
+    end
+  end
+
 end
