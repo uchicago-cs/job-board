bce23f2e798a2c6f33ebe292b9bbd56f70bcbe0c
Martin Posthumus <mposthumus@uchicago.edu>
2012-07-14 17:55:03 -0500

    Adding a few seeder tags (because the tag generation script behaves badly when there are no default tags)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+Tag.create(name: 'Ruby', tagtype: 'language')
+Tag.create(name: 'Java', tagtype: 'language')
+Tag.create(name: 'C', tagtype: 'language')
+Tag.create(name: 'Python', tagtype: 'language')
