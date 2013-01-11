444b9bab2d6947f63a6d466506a115f9c13f1f57
Martin Posthumus <mposthumus@uchicago.edu>
2013-01-11 12:04:50 -0600

    Adding log-in page intercepts of requests to pages with restricted permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,15 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    render :text => "Access denied.", :status => 403
+    if current_student || current_employer
+      render :text => "Access denied.", :status => 403
+    else
+      session[:requested_path] = request.fullpath
+      @student = Student.new
+      @employer = Employer.new
+      flash[:alert] = "You must log in to view this page"
+      render "misc/combined_login"
+    end
   end
 
   def current_ability
@@ -17,6 +25,17 @@ class ApplicationController < ActionController::Base
 
   def deobfuscate_id
     params[:id] = Obfuscation.deobfuscate(params[:id].to_i) if params[:id]
+  end
+
+  def after_sign_in_path_for(user)
+    dest = nil
+    if session[:requested_path]
+      dest = session[:requested_path]
+      session[:requested_path] = nil
+    else
+      dest = root_path
+    end
+    dest
   end
 
 end
