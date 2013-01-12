decf6b8f641b01c5f5a9159edee2fce61bd63f99
Martin Posthumus <mposthumus@uchicago.edu>
2013-01-11 19:18:51 -0600

    Updating production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ TechJobs::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.cs.uchicago.edu',
-    :port                 => 587,
+    :port                 => 25,
     :domain               => 'cs.uchicago.edu',
     :user_name            => 'jobboard-notifications',
     :password             => '',
