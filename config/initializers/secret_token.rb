acb617c414667312a19861daf969b36501e0333d
Martin Posthumus <mposthumus@uchicago.edu>
2013-03-07 00:37:34 -0600

    Removing session secret token from repo (and changing it)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TechJobs::Application.config.secret_token = '170b4f6e0e3d2217a6d9d405af342e044a1a9dc49990b2c256dc779f0bdbb1f16f7cbbd97ba0419c702af11c9aed485a48ab41c582916ef3f257dca0d1cb19a6'
+TechJobs::Application.config.secret_token = '[SESSION KEY WILL AUTOMATICALLY BE INSERTED HERE]'
