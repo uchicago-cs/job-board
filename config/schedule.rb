10cc5c523847fe50efb02341526e47bf8656c175
Martin Posthumus <mposthumus@uchicago.edu>
2013-03-22 06:27:18 -0500

    Explicitly defining bundle path in whenever config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+job_type :rake, "cd :path && RAILS_ENV=:environment /usr/local/bin/bundle exec rake :task --silent :output"
+
 every :friday, :at => '9pm' do
   rake "alerts:send_student_digests"
 end
