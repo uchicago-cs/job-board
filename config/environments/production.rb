9e75c73e83675bf3923fdbc87bb05ffb212ef5b1
Borja Sotomayor <borja@cs.uchicago.edu>
2014-07-09 16:03:25 -0500

    Changed SMTP port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ TechJobs::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.cs.uchicago.edu',
-    :port                 => 25,
+    :port                 => 587,
     :domain               => 'cs.uchicago.edu',
     :authentication       => nil,
     :enable_starttls_auto => true
